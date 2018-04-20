f3a24a3f1445f7628bfe27c52e7c3b5ee069a31c
zhezhang77 <zzonline@163.com>
2018-04-20 15:41:38 +1200

    Fix grovepi IOError

--- iot_pub.py
+++ iot_pub.py
@@ -17,155 +17,154 @@ running = True
 
 
 def handle_exit(signal, frame):
-    global running
-    print('Ctrl+C pressed')
-    running = False
+	global running
+	print('Ctrl+C pressed')
+	running = False
 
 # read config file
 
 
 def init():
-    conf_str = open(config_file).read()
-    # print(conf_str)
-    return json.loads(conf_str)
+	conf_str = open(config_file).read()
+	# print(conf_str)
+	return json.loads(conf_str)
 
 
 def sense_get_temperature(sense):
-    temp = sense.get_temperature()
-    cpu_temp = subprocess.check_output("vcgencmd measure_temp", shell=True)
-    array = cpu_temp.split("=")
-    array2 = array[1].split("'")
-    cpu_tempf = float(array2[0])
-    temp_calibrated = temp - ((cpu_tempf - temp)/2)
+	temp = sense.get_temperature()
+	cpu_temp = subprocess.check_output("vcgencmd measure_temp", shell=True)
+	array = cpu_temp.split("=")
+	array2 = array[1].split("'")
+	cpu_tempf = float(array2[0])
+	temp_calibrated = temp - ((cpu_tempf - temp)/2)
 
-    # print('CPU_TEMP = ' + repr(cpu_tempf)) #debug
-    # print('OLD_TEMP = ' + repr(temp)) #debug
-    # print('NEW_TEMP = ' + repr(temp_calibrated)) #debug
+	# print('CPU_TEMP = ' + repr(cpu_tempf)) #debug
+	# print('OLD_TEMP = ' + repr(temp)) #debug
+	# print('NEW_TEMP = ' + repr(temp_calibrated)) #debug
 
-    return float("{0:.1f}".format(temp_calibrated))
+	return float("{0:.1f}".format(temp_calibrated))
 
 
 def sense_get_humidity(sense):
-    return float("{0:.1f}".format(sense.get_humidity()))
+	return float("{0:.1f}".format(sense.get_humidity()))
 
 
 def sense_get_pressure(sense):
-    return float("{0:.0f}".format(sense.get_pressure()))
+	return float("{0:.0f}".format(sense.get_pressure()))
 
 
 def sense_get_compass(sense):
-    return float("{0:.0f}".format(sense.get_compass()))
+	return float("{0:.0f}".format(sense.get_compass()))
 
 # The callback for when the client receives a CONNACK response from the server.
 
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    # client.subscribe("$SYS/#")
+	print("Connected with result code "+str(rc))
+	# Subscribing in on_connect() means that if we lose the connection and
+	# reconnect then subscriptions will be renewed.
+	# client.subscribe("$SYS/#")
 
 # The callback for when a PUBLISH message is received from the server.
 
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+	print(msg.topic+" "+str(msg.payload))
 
 # Help function
 
 
 def on_log(client, userdata, level, buf):
-    print("Debug %d: %s" % (level, buf))
+	print("Debug %d: %s" % (level, buf))
 
 # Generate message payload string
 
 
 def gen_payload(id, name, value):
-    data = {}
-    data['id'] = id
-    data['time'] = time.time()
-    data[name] = value
-    return json.dumps(data)
+	data = {}
+	data['id'] = id
+	data['time'] = time.time()
+	data[name] = value
+	return json.dumps(data)
 
 # Genarate message topic
 
 
 def gen_topic(id, name):
-    return("iot/"+name+'/'+id)
+	return("iot/"+name+'/'+id)
 
 # Publich message
 
 
 def publish(client, id, name, value):
-    client.publish(gen_topic(id, name), gen_payload(id, name, value))
+	client.publish(gen_topic(id, name), gen_payload(id, name, value))
 
 
 def main():
-    global running
+	global running
 
-    signal.signal(signal.SIGINT, handle_exit)
+	signal.signal(signal.SIGINT, handle_exit)
 
-    # Read config file
-    conf = init()
+	# Read config file
+	conf = init()
 
-    # Connect to broker
-    client = mqtt.Client(conf['id'])
+	# Connect to broker
+	client = mqtt.Client(conf['id'])
 
-    client.tls_set("./cert/CARoot.pem",
-                   "./cert/"+conf['id']+"-certificate.pem.crt",
-                   "./cert/"+conf['id']+"-private.pem.key",
-                   cert_reqs=ssl.CERT_NONE,
-                   tls_version=ssl.PROTOCOL_TLSv1_2)
+	client.tls_set("./cert/CARoot.pem",
+				   "./cert/"+conf['id']+"-certificate.pem.crt",
+				   "./cert/"+conf['id']+"-private.pem.key",
+				   cert_reqs=ssl.CERT_NONE,
+				   tls_version=ssl.PROTOCOL_TLSv1_2)
 
-    client.on_connect = on_connect
-    client.on_message = on_message
-    # client.on_log = on_log #debug
+	client.on_connect = on_connect
+	client.on_message = on_message
+	# client.on_log = on_log #debug
 
-    client.connect(conf['entrypoint'], conf['port'], 60)
+	client.connect(conf['entrypoint'], conf['port'], 60)
 
-    # Init hardware
-    if (conf['type'] == 'sensehat'):
-        sense = SenseHat()
-        sense.set_imu_config(True, False, False)  # Campass Only
-    elif (conf['type'] == 'grovepi'):
-        pinMode(conf['button'], "INPUT")
-        pinMode(conf['potentiometer'], "INPUT")
-        time.sleep(1)
-
-    # Publish
-    while running:
-        if (conf['type'] == 'sensehat'):
-            publish(client, conf['id'], "temperature",
-                    repr(sense_get_temperature(sense)))
-            publish(client, conf['id'], "humidity",
-                    repr(sense_get_humidity(sense)))
-            publish(client, conf['id'], "pressure",
-                    repr(sense_get_pressure(sense)))
-            publish(client, conf['id'], "compass",
-                    repr(sense_get_compass(sense)))
-
-            eventList = sense.stick.get_events()
-            for event in eventList[:]:
-                if (event.direction == 'middle' and
-		    (event.action == 'pressed' or
-		     event.action == 'released')):
-                    publish(client, conf['id'], "button", event.action)
-	    time.sleep(1)
-
+	# Init hardware
+	if (conf['type'] == 'sensehat'):
+		sense = SenseHat()
+		sense.set_imu_config(True, False, False)  # Campass Only
 	elif (conf['type'] == 'grovepi'):
-            [temp, hum] = dht(conf['dht_port'], conf['dht_type'])
-	    potentiometer = analogRead(conf['potentiometer'])
-            button = digitalRead(conf['button'])
-            time.sleep(1)
+		pinMode(conf['button'], "INPUT")
+		pinMode(conf['potentiometer'], "INPUT")
+		time.sleep(1)
 
-            publish(client, conf['id'], "temperature",   repr(temp))
-            publish(client, conf['id'], "humidity",      repr(hum))
-            publish(client, conf['id'], "button",        repr(button))
-            publish(client, conf['id'], "potentiometer", repr(potentiometer))
+	# Publish
+	while running:
+		if (conf['type'] == 'sensehat'):
+			publish(client, conf['id'], "temperature",
+					repr(sense_get_temperature(sense)))
+			publish(client, conf['id'], "humidity",
+					repr(sense_get_humidity(sense)))
+			publish(client, conf['id'], "pressure",
+					repr(sense_get_pressure(sense)))
+			publish(client, conf['id'], "compass",
+					repr(sense_get_compass(sense)))
 
-    # Disconnect
-    client.disconnect()
+			eventList = sense.stick.get_events()
+			for event in eventList[:]:
+				if (event.direction == 'middle' and
+					(event.action == 'pressed' or
+					event.action == 'released')):
+					publish(client, conf['id'], "button", event.action)
+			time.sleep(1)
+		elif (conf['type'] == 'grovepi'):
+			[temp, hum] = dht(conf['dht_port'], conf['dht_type'])
+			potentiometer = analogRead(conf['potentiometer'])
+			button = digitalRead(conf['button'])
+			time.sleep(1)
+
+			publish(client, conf['id'], "temperature",   repr(temp))
+			publish(client, conf['id'], "humidity",      repr(hum))
+			publish(client, conf['id'], "button",        repr(button))
+			publish(client, conf['id'], "potentiometer", repr(potentiometer))
+
+	# Disconnect
+	client.disconnect()
 
 
 if __name__ == '__main__':
-    main()
+	main()
--- iot_sub.py
+++ iot_sub.py
@@ -18,242 +18,231 @@ config_file = 'iot_sub.conf'
 disp_data = {}  # dict
 disp_type = 0   # current display item num
 running = True  # control if program should exit
+grove_light = 0 # light sensor port
 
 # Notify thread to exit when receive Ctrl-C from console
 
 
 def handle_exit(signal, frame):
-    global running
-    print('Ctrl+C pressed')
-    running = False
+	global running
+	print('Ctrl+C pressed')
+	running = False
 
 # read config file
 
 
 def init():
-    conf_str = open(config_file).read()
-    return json.loads(conf_str)
+	conf_str = open(config_file).read()
+	return json.loads(conf_str)
 
 # The callback for when the client receives a CONNACK response from the server.
 
 
 def on_connect(client, userdata, flags, rc):
-    print('Connected with result code '+str(rc))
+	print('Connected with result code '+str(rc))
 
 # The callback for when the client disconnect from the server.
 
 
 def on_disconnect(client, userdata, rc):
-    print('Disconnected with result code '+str(rc))
+	print('Disconnected with result code '+str(rc))
 
 # The callback for when a PUBLISH message is received from the server.
 
 
 def on_message(client, userdata, msg):
-    global disp_data
+	global disp_data
 
-    # print(msg.topic+' '+str(msg.payload))#debug
-    data = json.loads(msg.payload)
-    # print(data) #debug
+	# print(msg.topic+' '+str(msg.payload))#debug
+	data = json.loads(msg.payload)
+	# print(data) #debug
 
-    m = re.search(r'(?<=/)\w+', msg.topic)
+	m = re.search(r'(?<=/)\w+', msg.topic)
 
-    disp_data[m.group(0)] = data[m.group(0)]
+	disp_data[m.group(0)] = data[m.group(0)]
 
 # Helper function
 
 
 def on_log(client, userdata, level, buf):
-    print('Debug %d: %s' % (level, buf))
+	print('Debug %d: %s' % (level, buf))
 
 # Increase disp_type when receiving specific event
 
 
 def sense_inc_disp_type(event):
-    global disp_type
-    if event.action != ACTION_RELEASED:
-        disp_type = disp_type + 1
+	global disp_type
+	if event.action != ACTION_RELEASED:
+		disp_type = disp_type + 1
 
 
 # Color table for different type of data
 color_table = [
-    [255, 0, 0],
-    [0, 255, 0],
-    [0, 0, 255],
-    [255, 255, 0],
-    [255, 0, 255],
-    [0, 255, 255],
-    [255, 255, 255]]
+	[255, 0, 0],
+	[0, 255, 0],
+	[0, 0, 255],
+	[255, 255, 0],
+	[255, 0, 255],
+	[0, 255, 255],
+	[255, 255, 255]]
 
 # display data on sense hat
 
 
 def sense_display_info(sense):
-    global disp_data
-    global disp_type
-    global color_table
-    global running
+	global disp_data
+	global disp_type
+	global color_table
+	global running
 
-    while running:
-        # Rotation check
-        x, y, z = sense.get_accelerometer_raw().values()
+	while running:
+		# Rotation check
+		x, y, z = sense.get_accelerometer_raw().values()
 
-        x = round(x, 0)
-        y = round(y, 0)
-        if x == -1:
-            sense.set_rotation(180)
-        elif y == -1:
-            sense.set_rotation(90)
-        elif y == 1:
-            sense.set_rotation(270)
-        else:
-            sense.set_rotation(0)
+		x = round(x, 0)
+		y = round(y, 0)
+		if x == -1:
+			sense.set_rotation(180)
+		elif y == -1:
+			sense.set_rotation(90)
+		elif y == 1:
+			sense.set_rotation(270)
+		else:
+			sense.set_rotation(0)
 
-        if (len(disp_data) > 0):
-            if (disp_type >= len(disp_data)):
-                disp_type = 0
+		if (len(disp_data) > 0):
+			if (disp_type >= len(disp_data)):
+				disp_type = 0
 
-            name = disp_data.keys()[disp_type]
-            value = disp_data[name]
-            sense.show_message(
-                name+':'+value, text_colour=color_table[disp_type % len(color_table)])
+			name = disp_data.keys()[disp_type]
+			value = disp_data[name]
+			sense.show_message(
+				name+':'+value,
+		text_colour=color_table[disp_type % len(color_table)])
 
-        time.sleep(1)
+		time.sleep(1)
 
-    sense.clear()
+	sense.clear()
 
 # display data on grovepi
 
 
 def grove_display_info():
-    global disp_data
-    global disp_type
-    global running
+	global disp_data
+	global disp_type
+	global running
+	global grove_light
 
-    curr_disp_type = -1
-    setText('')
-    setRGB(0, 0, 0)
+	curr_disp_type = -1
+	setText('>Waiting Data')
+	setRGB(255, 255, 255)
 
-    while running:
-        if (len(disp_data) > 0):
-            if (disp_type >= len(disp_data)):
-                disp_type = 0
+	while running:
+		# print('running...')
+		light = analogRead(grove_light)
+		if (light < 350):
+			light_trigger = light_trigger + 1
+		else:
+			light_trigger = 0
 
-            name = disp_data.keys()[disp_type]
-            value = disp_data[name]
-            disp_str = name + ': ' + value
-        else:
-            disp_str = '> No data'
+		if (light_trigger == 1):
+			disp_type = disp_type + 1
+		#print('light='+repr(light))
+		#print('light_trigger='+repr(light_trigger))
 
-        if (curr_disp_type != disp_type):
-            curr_disp_type = disp_type
-            setRGB(color_table[disp_type][0],
-                   color_table[disp_type][1],
-                   color_table[disp_type][2])
-        setText_norefresh(disp_str)
-        time.sleep(1)
+		if (len(disp_data) > 0):
+			if (disp_type >= len(disp_data)):
+				disp_type = 0
 
-    setRGB(0, 0, 0)
-    setText('')
+			name = disp_data.keys()[disp_type]
+			value = disp_data[name]
+			disp_str = name + ': ' + value
+		else:
+			disp_str = '>Waiting Data'
 
-# message thread function
+		# print(repr(curr_disp_type)+' '+repr(disp_type))
 
+		if (curr_disp_type != disp_type):
+			curr_disp_type = disp_type
+			setRGB(color_table[disp_type][0],
+						color_table[disp_type][1],
+						color_table[disp_type][2])
+		setText(disp_str)
+		time.sleep(1)
 
-def message_loop(client):
-    global running
-
-    while running:
-        client.loop()
-        time.sleep(1)
-
-    client.disconnect()
+	setRGB(0, 0, 0)
+	setText('')
 
 
 def main():
-    global running
-    global disp_type
+	global running
+	global disp_type
+	global grove_light
 
-    # Set signal
-    signal.signal(signal.SIGINT, handle_exit)
+	# Set signal
+	signal.signal(signal.SIGINT, handle_exit)
 
-    # Read config file
-    conf = init()
+	# Read config file
+	conf = init()
 
-    # Init hardware
-    if (conf['type'] == 'sensehat'):
-        sense = SenseHat()
+	# Init hardware
+	if (conf['type'] == 'sensehat'):
+		sense = SenseHat()
 
-        # Bind display item switching to up/down/left/right button
-        sense.stick.direction_up = sense_inc_disp_type
-        sense.stick.direction_down = sense_inc_disp_type
-        sense.stick.direction_left = sense_inc_disp_type
-        sense.stick.direction_right = sense_inc_disp_type
+		# Bind display item switching to up/down/left/right button
+		sense.stick.direction_up = sense_inc_disp_type
+		sense.stick.direction_down = sense_inc_disp_type
+		sense.stick.direction_left = sense_inc_disp_type
+		sense.stick.direction_right = sense_inc_disp_type
 
-        # Create display thread
-        disp_thread = threading.Thread(
-            target=sense_display_info, args=(sense,))
-    elif (conf['type'] == 'grovepi'):
-        # Set port mode
-        pinMode(conf['light'], "INPUT")
-        pinMode(conf['button'], "INPUT")
-        grove_light = conf['light'] - 14
-        # grove_button = conf['button']
-        # Create display thread
-        disp_thread = threading.Thread(target=grove_display_info)
+		# Create display thread
+		disp_thread = threading.Thread(
+			target=sense_display_info, args=(sense,))
+	elif (conf['type'] == 'grovepi'):
+		# Set port mode
+		pinMode(conf['light'], "INPUT")
+		pinMode(conf['button'], "INPUT")
+		grove_light = conf['light'] - 14
+		# grove_button = conf['button']
+		# Create display thread
+		disp_thread = threading.Thread(target=grove_display_info)
 
-    # Start display thread
-    disp_thread.daemon = False
-    disp_thread.start()
+	# Start display thread
+	disp_thread.daemon = False
+	disp_thread.start()
 
-    # Set connection
-    client = mqtt.Client(conf['id'])
-    client.tls_set('./cert/CARoot.pem',
-                   './cert/'+conf['id']+'-certificate.pem.crt',
-                   './cert/'+conf['id']+'-private.pem.key',
-                   cert_reqs=ssl.CERT_NONE,
-                   tls_version=ssl.PROTOCOL_TLSv1_2)
+	# Set connection
+	client = mqtt.Client(conf['id'])
+	client.tls_set('./cert/CARoot.pem',
+				   './cert/'+conf['id']+'-certificate.pem.crt',
+				   './cert/'+conf['id']+'-private.pem.key',
+				   cert_reqs=ssl.CERT_NONE,
+				   tls_version=ssl.PROTOCOL_TLSv1_2)
 
-    # Set client callback
-    client.on_connect = on_connect
-    client.on_disconnect = on_disconnect
-    client.on_message = on_message
-    # client.on_log = on_log #debug
+	# Set client callback
+	client.on_connect = on_connect
+	client.on_disconnect = on_disconnect
+	client.on_message = on_message
+	# client.on_log = on_log #debug
 
-    # Connect to broker
-    client.connect(conf['entrypoint'], conf['port'], 60)
+	# Connect to broker
+	client.connect(conf['entrypoint'], conf['port'], 60)
 
-    # Subscribe to topics
-    client.subscribe('iot/+/'+conf['display'])
+	# Subscribe to topics
+	client.subscribe('iot/+/'+conf['display'])
 
-    # Create message thread
-    client.loop_start()
+	# Create message thread
+	client.loop_start()
 
-    light_trigger = 0
+	light_trigger = 0
 
-    # deal with input to toggle disp_type
-    while running:
-        if (conf['type'] == 'grovepi'):
-            # button = digitalRead(grove_button)
-            # if (button == 1):
-            #     button_trigger = button_trigger + 1
-            # else:
-            #     button_trigger = 0
-            # print('button:'+repr(button)+' '+repr(button_trigger))
+	# deal with input to toggle disp_type
+	while running:
+		time.sleep(1)
 
-            light = analogRead(grove_light)
-            if (light < 50):
-                light_trigger = light_trigger + 1
-            else:
-                light_trigger = 0
-
-            if (light_trigger == 1):
-                disp_type = disp_type + 1
-        else:
-            time.sleep(1)
-
-    # Stop the loop then disconnect from broker
-    client.loop_stop()
-    client.disconnect()
+	# Stop the loop then disconnect from broker
+	client.loop_stop()
+	client.disconnect()
 
 if __name__ == '__main__':
-    main()
+	main()
